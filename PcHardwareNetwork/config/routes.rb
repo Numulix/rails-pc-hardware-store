9199d534821eafcd6c38578fb692bfcbb8eb435d
Jovan Babic <jbabic999@gmail.com>
2021-02-08 15:33:55 +0100

    Pages for all users and for individual users

--- PcHardwareNetwork/config/routes.rb
+++ PcHardwareNetwork/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   resources :cases
   resources :power_supplies
@@ -9,5 +10,7 @@ Rails.application.routes.draw do
   resources :processor_coolers
   resources :processors
   root 'static_pages#home'
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
