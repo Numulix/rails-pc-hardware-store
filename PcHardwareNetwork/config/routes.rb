a4b310832a57330f7d0c630831e63e0d16c51e45
Jovan Babic <jbabic999@gmail.com>
2021-02-08 13:36:24 +0100

    Added users to db using devise

--- PcHardwareNetwork/config/routes.rb
+++ PcHardwareNetwork/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :cases
   resources :power_supplies
   resources :video_cards
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
   resources :motherboards
   resources :processor_coolers
   resources :processors
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
